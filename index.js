7c9aa3b03ae8a6558d88d91b507716e8a341c160
Zakhe <zakhenet@gmail.com>
2025-12-11 18:28:57 +0200

    NAME Changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('I AM BAD SANTA!!!')
+  res.send('HELLO ZAKHE!!')
 })
 
 app.listen(PORT,()=>console.log("Port:"+PORT+" is Live"))
