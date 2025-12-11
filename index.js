e45523dafd302be5c5e81f76b697ba550732e4ca
Zakhe <zakhenet@gmail.com>
2025-12-11 19:00:19 +0200

    glitchy update

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 
+const UserSchema = new mongoose.Schema({name:String,password:Number})
+const User = mongoose.model("User",UserSchema,"users")
+
+async function ConnectDB(){
+    try{
+        await mongoose.connect(process.env.DBString)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
 const PORT = process.env.PORT || 5000
+ConnectDB()
+mongoose.connection.once("open",()=>{
+                console.log("DB Connected!")
+                app.listen(PORT,()=>console.log("Port:"+PORT+" is Live"))
+            })
 
 app.get('/', (req, res) => {
-  res.send('HELLO ZAKHE!!')
+
+    console.log(User.findOne({name:"Hero"}));
+    
+  res.send('HELLO KING!!')
 })
 
-app.listen(PORT,()=>console.log("Port:"+PORT+" is Live"))
